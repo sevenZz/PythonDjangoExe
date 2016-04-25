a200ab5c3824e4f91b631b242150a661d033538e
ansheng <zhang469249956@126.com>
2016-04-25 10:55:54 +0800

    init commit

---
+++ article/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from article.models import Article
+
+# Create your views here.
+def home(request):
+	return HttpResponse("Hello, Django")
+
+def detail(request, my_args):
+	post = Article.objects.all()[int(my_args)]
+	str = ("title = %s, category = %s, date_time = %s, content = %s" %(post.tile, post.category, post.date_time, post.content))
+	return HttpResponse(str)
